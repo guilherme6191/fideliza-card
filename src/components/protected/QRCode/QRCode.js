118c1e60a0a5421619d65874289da68e75a6533f
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2017-11-24 21:15:48 -0200

    fix: small styling change

---
+++ src/components/protected/QRCode/QRCode.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QRCode extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>QR Code</h1>
+      </div>
+    );
+  }
+}
+
+export default QRCode;
---
+++ src/components/protected/Reports/Reports.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reports extends Component {
+  render() {
+    return (
+       <div className="container">
+        <h1>Informações</h1>
+      </div>
+    );
+  }
+}
+
+export default Reports;
